c85ba5bf65ccafb48abf5bb5fac2ac37ce815ae9
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-09-22 18:02:19 -0400

    Add activities for Future and Present value
    Activities are empty and unreachable. Just added for the sake of having them at the
    moment.

---
+++ app/src/main/java/ca/qc/cegep_heritage/presentandfuturevaluecalculator/FutureValue.java
@@ -0,0 +1,13 @@
+package ca.qc.cegep_heritage.presentandfuturevaluecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FutureValue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_future_value);
+    }
+}
